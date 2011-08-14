882d0f4e9ecd0734fbbe44a0b4d8bee78407ddc3
mitts8 <mitts.ninja@gmail.com>
2011-08-13 18:36:25 -0700

    After created scaffolds for User & Micropost

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'bbfd973032c5d260970280851bae1b0ee771888d162826e2693224040a2093eed5d3c01188b7d72afd8cd5451bc9ff0af5776e060777fd364d9de1a279ae5f50'
